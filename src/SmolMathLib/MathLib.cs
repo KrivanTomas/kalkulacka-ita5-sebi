105df1d239e6aa7f4088126eb58cef1fb319e71b
Tomáš Křivan <cyan.roke@gmail.com>
2025-03-19 17:59:50 +0100

    Add MathLib function definitions

---
+++ src/SmolMathLib/MathLib.cs
@@ -0,0 +1,99 @@
+
+namespace SmolMathLib
+{
+    /// <summary>
+    /// A static class containing mathematical functions
+    /// </summary>
+    public static class MathLib
+    {
+
+        /// <summary>
+        /// Adds the two numbers
+        /// </summary>
+        /// <param name="a">First operand</param>
+        /// <param name="b">Second operand</param>
+        /// <returns>Sum of the two numbers</returns>
+        public static double Add(double a, double b)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Subdivides the second number from the first
+        /// </summary>
+        /// <param name="a">First operand</param>
+        /// <param name="b">Second operand</param>
+        /// <returns>Difference between the two numbers</returns>
+        public static double Sub(double a, double b)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Multiplies the two numbers
+        /// </summary>
+        /// <param name="a">First operand</param>
+        /// <param name="b">Second operand</param>
+        /// <returns>Product of the two numbers</returns>
+        public static double Mul(double a, double b)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Divides the dividend by the divisor
+        /// </summary>
+        /// <param name="a">Dividend</param>
+        /// <param name="b">Divisor</param>
+        /// <exception cref="DivideByZeroException"/>
+        /// <returns>Quotient of the operation</returns>
+        public static double Div(double a, double b)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Product of all numbers smaller or equal to <c>n</c>
+        /// </summary>
+        /// <param name="n">Factorial input</param>
+        /// <returns>Factorial of n</returns>
+        public static ulong Fac(int n)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Exponentiation of <c>b</c> to the power of <c>e</c>
+        /// </summary>
+        /// <param name="b">Base</param>
+        /// <param name="e">Exponent (power)</param>
+        /// <returns>Base to the power of the exponent</returns>
+        public static double Pow(double b, int e)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="r">Radicand</param>
+        /// <param name="d">Degree</param>
+        /// <returns></returns>
+        public static double Root(double r, double d)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Log of <c>b</c>(base) and <c>a</c>(anti-logarithm)
+        /// </summary>
+        /// <param name="b">Base</param>
+        /// <param name="a">Anti-logarithm</param>
+        /// <returns>Logarithm of a in base b</returns>
+        public static double Log(double b, double a)
+        {
+            return 0;
+        }
+    }
+
+}
