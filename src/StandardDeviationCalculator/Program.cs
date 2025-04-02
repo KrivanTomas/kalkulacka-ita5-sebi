fbf8e03a37bb617faf2d94d454a53a30b44f0143
KurtinLukas <strilxlol@gmail.com>
2025-04-02 15:10:24 +0200

    Read numbers

--- src/StandardDeviationCalculator/Program.cs
+++ src/StandardDeviationCalculator/Program.cs
@@ -1,2 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// To-do: 
+//  SDC (Standard deviation calculator)
+//  Profiling
+
+using SmolMathLib;
+
+namespace StandardDeviationCalculator
+{
+    public static class StandardDeviationCalculator
+    {
+        
+        public static int Main()
+        {
+            int readChar; //input char
+            int readNum = 0;
+            double sum = 0;
+            int numCount = 0;
+            double s = 0; //Standard deviation
+            readChar = Console.Read();
+
+            while(true)
+            {
+                if ((readChar < '0' || readChar > '9') && !Char.IsWhiteSpace((char)readChar) && readChar != '\0')
+                {
+                    Console.WriteLine("Error: input not number. Exiting\n\r");
+                    return 0;
+                }
+                if (!Char.IsWhiteSpace((char)readChar) && readChar != '\0')
+                {
+                    readNum = readNum * 10 + (readChar - '0');
+                }
+                else
+                {
+                    //full number read
+                    sum += readNum;
+                    readNum = 0;
+                    numCount++;
+
+                }
+                
+                if (readChar == '\0') //EOF
+                    break;
+                readChar = Console.Read();
+            }
+            Console.WriteLine("Směrodatná odchylka: " + sum);
+            return 0;
+        }
+
+        public static void Func(int cislo)
+        {
+            Console.WriteLine((char)cislo);
+            return;
+        }
+    }
+
+}
+
