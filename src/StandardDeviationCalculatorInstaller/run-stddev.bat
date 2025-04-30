fb91b08cf7643c7cb766726474f36965b81cc032
KurtinLukas <strilxlol@gmail.com>
2025-04-30 19:09:48 +0200

    Change project settings and dependencies

--- src/StandardDeviationCalculatorInstaller/run-stddev.bat
+++ src/StandardDeviationCalculatorInstaller/run-stddev.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Zadejte posloupnost cisel pro vypocet smerodatne odchylky. Ukoncete pomoci klavesy "CTRL+Z" na novem radku.
 .\stddev.exe
-set /p a=""
+set /p a="Stisknete enter pro ukonceni."
